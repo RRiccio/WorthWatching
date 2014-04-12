226e700dd027f754ed7216e941381825858ba4f2
Roberto Riccio <RRiccio@Robertos-MacBook-Pro.local>
2014-04-12 15:48:17 -0300

    Starting up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorthWatching::Application.config.secret_token = '22d90cd08b3c176925fb73e0c9cb15311cbdaa6bf3c3145d8910ed5f106805d09bee0eab8aa97b4405a3e094453af20ce44181ea3de33f1c026f4017d6125c10'
