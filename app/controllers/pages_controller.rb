6ed2c5b87fdac593fb07d7c5404ec29a1fb70b10
Thulio Costa <thulio.costa@gmail.com>
2014-04-12 23:09:36 -0300

    Rejected movies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
      @movies = Movie.fetch
-     @movies.reject! do |movie|
+     @rejected_movies, @movies = @movies.partition do |movie|
       movie["rating"].blank? || movie["rating_count"].blank? || movie["rating"] < 7.5 || movie["rating_count"] < 5000
      end
      @movies = @movies.sort_by{|movie| movie["theater_count"]}.reverse
